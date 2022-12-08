e4360007654a90552d76306260ea6f1a6963e3c3
mei <mei@trapasecurity.com>
2022-12-08 16:52:01 +0800

    feat: day4 - Christmas tree

---
+++ src/components/ChristmasTree.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ChristmasTree v-if="size > 1" :size="size - 1" />
+    <div class="flex flex-row justify-center">
+      <div
+        v-for="i in size"
+        :key="i"
+        class="relative flex items-center justify-center w-16 h-16 -m-2 bg-green-500 rounded-full"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  size: {
+    type: Number,
+    default: 1,
+  },
+});
+</script>
--- src/views/Day4.vue
+++ src/views/Day4.vue
@@ -1,1 +1,12 @@
-<template>day</template>
+<template>
+  <section class="mt-[300px] flex flex-col justify-center">
+    <h2 class="mb-20 text-4xl font-bold text-center">Christmas Tree</h2>
+    <div class="flex items-center justify-center w-full h-full">
+      <ChristmasTree :size="7" />
+    </div>
+  </section>
+</template>
+
+<script setup>
+import ChristmasTree from '@/components/ChristmasTree.vue';
+</script>
